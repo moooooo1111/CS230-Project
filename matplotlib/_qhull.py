0ca356286155aec6f60293a61ddbd7f6e7050c1e
moooooo1111 <100150198+moooooo1111@users.noreply.github.com>
2022-12-13 20:23:31 -0500

    Add files via upload

---
+++ matplotlib/_qhull.py
@@ -0,0 +1,37 @@
+# encoding: utf-8
+# module matplotlib._qhull calls itself qhull
+# from C:\Users\mr855\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\LocalCache\local-packages\Python310\site-packages\matplotlib\_qhull.cp310-win_amd64.pyd
+# by generator 1.147
+""" Computing Delaunay triangulations. """
+# no imports
+
+# functions
+
+def delaunay(*args, **kwargs): # real signature unknown
+    """
+    Compute a Delaunay triangulation.
+    
+    Parameters
+    ----------
+    x, y : 1d arrays
+        The coordinates of the point set, which must consist of at least
+        three unique points.
+    
+    Returns
+    -------
+    triangles, neighbors : int arrays, shape (ntri, 3)
+        Indices of triangle vertices and indices of triangle neighbors.
+    """
+    pass
+
+def version(*args, **kwargs): # real signature unknown
+    """ Return the qhull version string. """
+    pass
+
+# no classes
+# variables with complex values
+
+__loader__ = None # (!) real value is '<_frozen_importlib_external.ExtensionFileLoader object at 0x00000204AC4814E0>'
+
+__spec__ = None # (!) real value is "ModuleSpec(name='matplotlib._qhull', loader=<_frozen_importlib_external.ExtensionFileLoader object at 0x00000204AC4814E0>, origin='C:\\\\Users\\\\mr855\\\\AppData\\\\Local\\\\Packages\\\\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\\\\LocalCache\\\\local-packages\\\\Python310\\\\site-packages\\\\matplotlib\\\\_qhull.cp310-win_amd64.pyd')"
+
---
+++ matplotlib/_tri.py
@@ -0,0 +1,101 @@
+# encoding: utf-8
+# module matplotlib._tri
+# from C:\Users\mr855\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\LocalCache\local-packages\Python310\site-packages\matplotlib\_tri.cp310-win_amd64.pyd
+# by generator 1.147
+# no doc
+# no imports
+
+# no functions
+# classes
+
+class TrapezoidMapTriFinder(object):
+    """
+    Create a new C++ TrapezoidMapTriFinder object.
+    This should not be called directly, instead use the python class
+    matplotlib.tri.TrapezoidMapTriFinder instead.
+    """
+    def find_many(self, *args, **kwargs): # real signature unknown
+        """ Find indices of triangles containing the point coordinates (x, y). """
+        pass
+
+    def get_tree_stats(self, *args, **kwargs): # real signature unknown
+        """ Return statistics about the tree used by the trapezoid map. """
+        pass
+
+    def initialize(self, *args, **kwargs): # real signature unknown
+        """ Initialize this object, creating the trapezoid map from the triangulation. """
+        pass
+
+    def print_tree(self, *args, **kwargs): # real signature unknown
+        """ Print the search tree as text to stdout; useful for debug purposes. """
+        pass
+
+    def __init__(self, *args, **kwargs): # real signature unknown
+        pass
+
+    @staticmethod # known case of __new__
+    def __new__(*args, **kwargs): # real signature unknown
+        """ Create and return a new object.  See help(type) for accurate signature. """
+        pass
+
+
+class Triangulation(object):
+    """
+    Create a new C++ Triangulation object.
+    This should not be called directly, instead use the python class
+    matplotlib.tri.Triangulation instead.
+    """
+    def calculate_plane_coefficients(self, *args, **kwargs): # real signature unknown
+        """ Calculate plane equation coefficients for all unmasked triangles. """
+        pass
+
+    def get_edges(self, *args, **kwargs): # real signature unknown
+        """ Return edges array. """
+        pass
+
+    def get_neighbors(self, *args, **kwargs): # real signature unknown
+        """ Return neighbors array. """
+        pass
+
+    def set_mask(self, *args, **kwargs): # real signature unknown
+        """ Set or clear the mask array. """
+        pass
+
+    def __init__(self, *args, **kwargs): # real signature unknown
+        pass
+
+    @staticmethod # known case of __new__
+    def __new__(*args, **kwargs): # real signature unknown
+        """ Create and return a new object.  See help(type) for accurate signature. """
+        pass
+
+
+class TriContourGenerator(object):
+    """
+    Create a new C++ TriContourGenerator object.
+    This should not be called directly, instead use the functions
+    matplotlib.axes.tricontour and tricontourf instead.
+    """
+    def create_contour(self, *args, **kwargs): # real signature unknown
+        """ Create and return a non-filled contour. """
+        pass
+
+    def create_filled_contour(self, *args, **kwargs): # real signature unknown
+        """ Create and return a filled contour. """
+        pass
+
+    def __init__(self, *args, **kwargs): # real signature unknown
+        pass
+
+    @staticmethod # known case of __new__
+    def __new__(*args, **kwargs): # real signature unknown
+        """ Create and return a new object.  See help(type) for accurate signature. """
+        pass
+
+
+# variables with complex values
+
+__loader__ = None # (!) real value is '<_frozen_importlib_external.ExtensionFileLoader object at 0x00000257D53814E0>'
+
+__spec__ = None # (!) real value is "ModuleSpec(name='matplotlib._tri', loader=<_frozen_importlib_external.ExtensionFileLoader object at 0x00000257D53814E0>, origin='C:\\\\Users\\\\mr855\\\\AppData\\\\Local\\\\Packages\\\\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\\\\LocalCache\\\\local-packages\\\\Python310\\\\site-packages\\\\matplotlib\\\\_tri.cp310-win_amd64.pyd')"
+
